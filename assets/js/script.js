aef1957d4f987f6c43cff341ca6ca0beb24c723e
Lowari <66225181+Lowari@users.noreply.github.com>
2020-11-06 11:37:17 +0100

    Correction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
-var img = document.querySelector("#firstImg");
+const img = document.querySelector("#firstImg");
 
- 
- firstImg.addEventListener('click', function() {
-      firstImg.src = "assets/img/second.jpg";
-})
+
+img.onclick = function () {
+      if (img.src == "file:///C:/Users/julie/OneDrive/Bureau/Cours%20js%20la%20manu/Partie%205%20-%20Les%20%C3%A9v%C3%A9nements%20et%20la%20manipulation%20du%20DOM/Exo%204/assets/img/first.png") {
+            img.src = "assets/img/second.jpg";
+      } else {
+            img.src = "assets/img/first.png";
+      }
+}
+
+img.onmouseover = function () {
+      img.style.width = "40rem";
+}
+
+img.onmouseout = function () {
+      img.style.width = "30rem";
+}
